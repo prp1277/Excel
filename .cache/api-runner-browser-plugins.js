3a9e9c594edf1afe52bfc9507df4c9716de1a4fa
Patrick Powell <prp1277@gmail.com>
2019-09-08 17:17:20 -0500

    No more gatsby

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('c:/Users/prp12.000/GitHub-Repos/Excel/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('c:/Users/prp12.000/GitHub-Repos/Excel/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
